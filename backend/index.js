8f962ea3fb07cf827a7eaf406b823bb7f02cb75e
Bluekmags <kam00026@mix.wvu.edu>
2024-03-19 17:49:19 -0400

    worked on code

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-import express from "express"
-import mysql from "mysql"
-import cors from "cors"
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
 
 
 const app = express()
@@ -8,13 +8,13 @@ const app = express()
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"pancakes",
+    password:"ohnothereis1",
     database:"test"
-
 })
 
 app.use(express.json())
 app.use(cors())
+
 
 app.get("/", (req,res)=>{
     res.json("hello this is the backend")
@@ -22,14 +22,14 @@ app.get("/", (req,res)=>{
 
 app.get("/books", (req,res)=>{
     const q = "SELECT * FROM test.books"
-    db.query(q,(err,data)=>{
-       if(err) return res.json(err)
-       return res.json(data) 
+    db.query(q,(data,err)=>{
+        if (err) return res.json(err)
+        return res.json(data)
     })
 })
 
 app.post("/books", (req,res)=>{
-    const q = "INSERT INTO books (`title`,`desc`,`price`,`cover`) VALUES (?)";
+    const q = "INSERT INTO books (`title`,`desc`,`price`,`cover`) Values (?)"
     const values = [
         req.body.title,
         req.body.desc,
@@ -38,12 +38,12 @@ app.post("/books", (req,res)=>{
 
     ];
 
-db.query(q,[values], (err,data)=>{
-    if(err) return res.json(err);
-    return res.json("Book has been created successfully!");
-});
+    db.query(q,[values], (err,data)=>{
+        if(err) return res.json(err);
+        return res.json("Book has been created successfully!");
+    });
 
-});
+})
 
 app.delete("/books/:id", (req,res)=>{
     const bookId = req.params.id;
@@ -54,6 +54,7 @@ app.delete("/books/:id", (req,res)=>{
         return res.json("Book has been deleted successfully.")
     })
 })
+
 app.put("/books/:id", (req,res)=>{
     const bookId = req.params.id;
     const q = "UPDATE books SET `title` = ?, `desc` = ?, `price` = ?, `cover` = ? WHERE id = ?"
@@ -73,5 +74,5 @@ app.put("/books/:id", (req,res)=>{
 })
 
 app.listen(8800, ()=>{
-    console.log("Connected to backend!")
+    console.log("Connected to Backend!")
 })
